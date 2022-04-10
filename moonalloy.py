07cc2c9aac368e07469513b2989ac3e9b85b6d6e
HackTheOxidation <tomas.hagenau@protonmail.ch>
2022-04-10 14:47:04 +0200

    Added more ffi functions.

--- moonalloy.py
+++ moonalloy.py
@@ -17,14 +17,45 @@ class _Array_t(ctypes.Structure):
                 ('arr', ctypes.POINTER(ctypes.c_double))]
 
 
-# initialize all functions in moonalloy
+# Initialize all functions in moonalloy
+# array_print
 _moonalloy.array_print.argtypes = [ctypes.POINTER(_Array_t)]
 _moonalloy.array_print.restype = None
 
+# array_ones
 _moonalloy.array_ones.argtypes = [ctypes.c_int]
 _moonalloy.array_ones.restype = ctypes.POINTER(_Array_t)
 
+# array_zeros
+_moonalloy.array_zeros.argtypes = [ctypes.c_int]
+_moonalloy.array_zeros.restype = ctypes.POINTER(_Array_t)
+
+# array_sum
+_moonalloy.array_sum.argtypes = [ctypes.POINTER(_Array_t)]
+_moonalloy.array_sum.restype = ctypes.c_double
+
+# array_scalar
+_moonalloy.array_scalar.argtypes = [ctypes.POINTER(_Array_t), ctypes.c_double]
+_moonalloy.array_scalar.restype = ctypes.POINTER(_Array_t)
+
+# Test
+print("----- Initiating Test -----\n\n")
 
 arr = _moonalloy.array_ones(ctypes.c_int(3))
 print("Result: ")
 _moonalloy.array_print(arr)
+print()
+
+arr2 = _moonalloy.array_zeros(ctypes.c_int(2))
+print("Result: ")
+_moonalloy.array_print(arr2)
+print()
+
+sum_result = _moonalloy.array_sum(arr)
+print(f"Result: {sum_result}")
+print()
+
+scalar_result = _moonalloy.array_scalar(arr, ctypes.c_double(2.5))
+print("Result: ")
+_moonalloy.array_print(scalar_result)
+print()
